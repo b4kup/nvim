96e863ea2842107baeca134d97762ea961b84350
b4kup <justus.ernst@funkemedien.de>
2026-03-04 13:59:18 +0100

    gf dont close terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,20 @@
 
 vim.keymap.set("n", "<Esc>", "<cmd>w<CR>", { desc = "Save buffer" })
 
+-- In terminal buffers, gf opens file in previous (editor) window instead of closing the terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.keymap.set("n", "gf", function()
+      local file = vim.fn.expand("<cfile>")
+      if file == "" then
+        return
+      end
+      vim.cmd("wincmd p")
+      vim.cmd("edit " .. vim.fn.fnameescape(file))
+    end, { buffer = true, desc = "Go to file in editor window" })
+  end,
+})
+
 vim.keymap.set("n", "<leader>sn", function()
   local ft = vim.bo.filetype
   local path = vim.fn.stdpath("config") .. "/snippets/" .. ft .. ".json"
