fdab66717ff7a335203797013cc9986af5f4c43a
b4kup <justus.ernst@funkemedien.de>
2026-03-04 13:46:05 +0100

    dart: seprate run and debug

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -16,12 +16,35 @@ return {
         highlight = "Comment",
       },
       debugger = {
-        enabled = true,
+        enabled = false,
         evaluate_to_string_in_debug_views = true,
       },
     },
     keys = {
       { "<leader>Fa", "<cmd>FlutterRun<CR>", desc = "Flutter Run" },
+      {
+        "<leader>Fb",
+        function()
+          local ft_config = require("flutter-tools.config")
+          local dap = require("dap")
+
+          -- Suppress dev_log (dapui console already shows output)
+          ft_config.dev_log.enabled = false
+
+          -- Restore dev_log when DAP session ends
+          local key = "flutter_devlog_restore"
+          local function restore()
+            ft_config.dev_log.enabled = true
+            dap.listeners.before.event_terminated[key] = nil
+            dap.listeners.before.event_exited[key] = nil
+          end
+          dap.listeners.before.event_terminated[key] = restore
+          dap.listeners.before.event_exited[key] = restore
+
+          vim.cmd("FlutterDebug")
+        end,
+        desc = "Flutter Debug (DAP)",
+      },
       { "<leader>FR", "<cmd>FlutterRestart<CR>", desc = "Flutter Restart" },
       { "<leader>Fq", "<cmd>FlutterQuit<CR>", desc = "Flutter Quit" },
       { "<leader>FD", "<cmd>FlutterVisualDebug<CR>", desc = "Flutter Visual Debug" },
