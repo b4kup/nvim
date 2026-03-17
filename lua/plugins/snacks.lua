1921cdd44c23b4a41715da558c8451853e090585
b4kup <justus.ernst@funkemedien.de>
2026-03-17 10:09:52 +0100

    revert dual terminal

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -14,16 +14,5 @@ return {
         },
       },
     },
-    keys = {
-      {
-        "<leader>ft",
-        function()
-          -- Two side-by-side terminals in bottom pane
-          Snacks.terminal.toggle(nil, { count = 1 })
-          Snacks.terminal.toggle(nil, { count = 2 })
-        end,
-        desc = "Two terminals (bottom)",
-      },
-    },
   },
 }
