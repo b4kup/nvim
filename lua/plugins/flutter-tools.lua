846706d977eb2486b26ec5e03305348224f55e30
b4kup <justus.ernst@funkemedien.de>
2026-03-19 10:27:05 +0100

    flutter log colors

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -6,6 +6,70 @@ return {
       "nvim-lua/plenary.nvim",
       "stevearc/dressing.nvim",
     },
+    config = function(_, opts)
+      require("flutter-tools").setup(opts)
+
+      local color_map = {
+        [30] = "Comment",
+        [31] = "DiagnosticError",
+        [32] = "DiagnosticOk",
+        [33] = "DiagnosticWarn",
+        [34] = "DiagnosticInfo",
+        [35] = "Special",
+        [36] = "DiagnosticHint",
+        [37] = "Normal",
+      }
+      local ns = vim.api.nvim_create_namespace("flutter_log_colors")
+      local log = require("flutter-tools.log")
+      local original_log = log.log
+
+      log.log = function(data)
+        -- Parse ANSI color segments
+        local segments = {}
+        local current_hl = nil
+        local pos = 1
+        while true do
+          local s, e, codes = data:find("\027%[([%d;]*)m", pos)
+          if not s then
+            if pos <= #data then
+              table.insert(segments, { text = data:sub(pos), hl = current_hl })
+            end
+            break
+          end
+          if s > pos then
+            table.insert(segments, { text = data:sub(pos, s - 1), hl = current_hl })
+          end
+          local last_code = tonumber(codes:match("(%d+)$"))
+          if last_code == 0 or codes == "" then
+            current_hl = nil
+          elseif color_map[last_code] then
+            current_hl = color_map[last_code]
+          end
+          pos = e + 1
+        end
+
+        -- Build clean text (no ANSI codes) and write via original log
+        local clean = table.concat(vim.tbl_map(function(seg)
+          return seg.text
+        end, segments))
+        original_log(clean)
+
+        -- Apply highlights via extmarks (works on nomodifiable buffers)
+        if log.buf and vim.api.nvim_buf_is_valid(log.buf) then
+          local line = vim.api.nvim_buf_line_count(log.buf) - 1
+          local col = 0
+          for _, seg in ipairs(segments) do
+            if seg.hl and #seg.text > 0 then
+              pcall(vim.api.nvim_buf_set_extmark, log.buf, ns, line, col, {
+                end_col = col + #seg.text,
+                hl_group = seg.hl,
+              })
+            end
+            col = col + #seg.text
+          end
+        end
+      end
+    end,
     opts = {
       widget_guides = {
         enabled = true,
